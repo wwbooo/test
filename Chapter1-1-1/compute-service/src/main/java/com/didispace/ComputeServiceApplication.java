9cb1a843be5b3c5d65d6e69b783338c149b1bcc9
feelingoo <uioqop@163.com>
2018-08-04 17:02:19 +0800

    添加启动类的注释

--- Chapter1-1-1/compute-service/src/main/java/com/didispace/ComputeServiceApplication.java
+++ Chapter1-1-1/compute-service/src/main/java/com/didispace/ComputeServiceApplication.java
@@ -4,6 +4,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * @Description: 项目启动类
+ * @company Finedo.cn
+ * @author zhusf@finedo.com
+ * @version v1.0
+ */
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ComputeServiceApplication {
