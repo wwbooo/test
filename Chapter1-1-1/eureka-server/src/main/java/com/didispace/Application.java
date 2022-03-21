270021725a2ef74032140c76858a695d2b37ec1b
wwbooo <102027151+wwbooo@users.noreply.github.com>
2022-03-21 19:29:45 +0800

    Update Application.java
    add class

--- Chapter1-1-1/eureka-server/src/main/java/com/didispace/Application.java
+++ Chapter1-1-1/eureka-server/src/main/java/com/didispace/Application.java
@@ -11,5 +11,8 @@ public class Application {
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(Application.class).web(true).run(args);
 	}
+	public static void cc(String args) {
+		System.out.println("YES");
+	}
 
 }
